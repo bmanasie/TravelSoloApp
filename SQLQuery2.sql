16a4862ad5d2795d1126a243e681ffc1d1b68d71
Manasie Bajpai <mbaj0002@student.monash.edu>
2019-10-13 12:20:55 +1100

    Revert "New App"
    This reverts commit 0276172b418cef1df7b41fa063a0a3672dacbc56.

--- SQLQuery2.sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT INTO [dbo].[Location] ([Name], [Description], [Latitude],
-[Longitude]) VALUES (N'Melbourne', N'Melbourne office', CAST(-
-37.8136 AS Decimal(10, 8)), CAST(144.9631 AS Decimal(11, 8)))
-
